4499214f4b1934e31bd2a331bde6d0cb64063f00
Kaique <34290569+EuKaique@users.noreply.github.com>
2022-07-10 12:24:46 -0300

    Avançando no JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,9 +6,19 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return 'Login';
+        $credenciais = $request->all(['email', 'password']);
+
+        $token = auth('api')->attempt($credenciais);
+
+        if($token){
+            return response()->json(['token' => $token], 200);
+        }else{
+            return response()->json(['erro' => 'Usuário ou senha inválida'], 403);
+        }
+
+        $bearer = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTY1NzQ2MzgxOCwiZXhwIjoxNjU3NDY3NDE4LCJuYmYiOjE2NTc0NjM4MTgsImp0aSI6Ik95ODY4dXYya1lTT0x6VDQiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.gZcUc4MFgcz0FaomfobN2AeLuGsg6FboB_ENQiITy_I';
     }
 
     public function logout()
@@ -23,6 +33,6 @@ class AuthController extends Controller
 
     public function me()
     {
-        return 'Me';
+        return response()->json(auth()->user());
     }
 }
